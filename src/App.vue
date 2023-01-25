a010cd1068ff5e53e7c23b9451ec4dd0ae5fd1ed
Hacker God <jyrone.parker@gmail.com>
2023-01-25 03:28:22 -0500

    class is pretty solid.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,81 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span v-if="isLoading">
+      <h2> Generating Horror Short </h2>
+    </span>
+    <button @click="generateBook" v-if="readyToGenerate">Generate Horror Short</button>
+    <ul v-if="isPlaying">
+      <button @click="pauseStory" >Pause</button>
+      <button @click="stopStory" >Stop</button>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import OpenAi from './classes/OpenAi'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      isLoading: false,
+      isPlaying: false,
+      openAi: {},
+      books: [],
+      prompt: 'generate a 3 paragraph horror story and output an SSML file prefixing <?xml version="1.0" ?> that is serialized to a string.'
+    }
+  },
+  created () {
+    this.openAi = new OpenAi('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', {
+      pitch: 0.88,
+      rate: 0.88
+    })
+    this.openAi.onstorygenerated = () => {
+      this.isLoading = true
+    }
+    this.openAi.onspeechkitutterancestarted = (event) => {
+      console.log(event)
+    }
+  },
+  computed: {
+    readyToGenerate () {
+      return !this.isLoading && !this.isPlaying
+    }
+  },
+  methods: {
+    pauseStory () {
+      this.openAi.pauseStory()
+      this.isPlaying = false
+    },
+    stopStory () {
+      this.openAi.stopStory()
+      this.isPlaying = false
+    },
+    resumeStory () {
+      this.openAi.resumeStory()
+      this.isPlaying = true
+    },
+     generateBook () {
+      const that = this
+      that.isLoading = true
+      const promise = new Promise(function(resolve, reject) {
+      try {
+        that.openAi.readStory(that.prompt)
+        resolve(); // Yay! Everything went well!
+      } catch (e) {
+        reject(e)
+        }
+      })
+      promise.then(() => {
+        that.isPlaying = true
+        that.isLoading = false
+      }).catch((e) => {
+        alert(e.message)
+        that.isPlaying = false
+        that.isLoading = false
+      })
+      return promise
+    }
   }
 }
 </script>
---
+++ src/classes/OpenAi.js
@@ -0,0 +1,85 @@
+import SpeechKit from '@mastashake08/speech-kit'
+export default class OpenAi extends EventTarget {
+
+  constructor (apiKey, speechOptions = {}) {
+    super()
+    this.apiKey = apiKey
+    this.sk = new SpeechKit(speechOptions)
+    this.bookSSML = null
+    this.baseUrl = 'https://api.openai.com/v1/completions'
+    this.books = {}
+    this.booksReady = false
+  }
+
+  async generateBook (data = {}) {
+    try {
+      const res = await fetch(this.baseUrl, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${this.apiKey}`
+        },
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+      });
+      this.books = await res.json()
+      this.saveStory(this.books.choices[0])
+      this.booksReady = true
+      this.dispatchEvent(new Event('storygenerated'))
+    } catch (e) {
+      alert(e.message)
+      this.booksReady = false
+      this.dispatchEvent(new Event('storygeneratederror'))
+    }
+  }
+
+  pauseStory () {
+    this.sk.pauseSynth()
+  }
+
+  stopStory () {
+    this.sk.stopSynth()
+  }
+
+  async readStory (prompt) {
+    try {
+      this.addEventListener('onspeechkitutterencestart', function () {
+        this.isPlaying =  true
+      })
+      this.addEventListener('onspeechkitutterenceend', function () {
+        this.isPlaying =  false
+      })
+      const data = { model: "text-davinci-003", prompt: prompt, temperature: 0.9, max_tokens: 500}
+      await this.generateBook(data)
+      if(this.booksReady) {
+        const ssml = this.books.choices[0].text.trim()
+        this.sk.speak(ssml)
+        this.dispatchEvent(new Event('storyreadgenerated'))
+      } else {
+        throw new Error('ChatGPT is busy right now. Please try again in a few minutes!')
+        }
+      } catch (e) {
+      alert(e.message)
+      this.dispatchEvent(new Event('storyreaderror'))
+    }
+  }
+
+  convertToText(book) {
+    const text = book.text.replace(/<\/?[^>]+(>|$)/g, "")
+    return text.trim()
+  }
+
+  saveStory(book) {
+    try {
+      if(localStorage.books !== null) {
+        const books = JSON.parse(localStorage.books)
+        books.push(book)
+        localStorage.books = JSON.stringify(books)
+      } else {
+        localStorage.books = JSON.stringify([book])
+      }
+    } catch (e) {
+      console.log(e)
+      return
+    }
+  }
+}
