28cc1a428d4cc3b9215329844a5c1ced0792830b
Hacker God <jyrone.parker@gmail.com>
2023-01-27 00:17:37 -0500

    PWA

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Ads from 'vue-google-adsense'
+import './registerServiceWorker'
 Vue.config.productionTip = false
 Vue.use(require('vue-script2'))
 Vue.use(Ads.Adsense)
